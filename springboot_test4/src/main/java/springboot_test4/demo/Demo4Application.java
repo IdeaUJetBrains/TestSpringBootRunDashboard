da0722531301aab3be91617d5740177520d0e30f
OlgaJP <olga.pavlova@jetbrains.com>
2017-06-30 10:04:09 +0200

    changed a class name

--- springboot_test4/src/main/java/springboot_test4/demo/Demo4Application.java
+++ springboot_test4/src/main/java/springboot_test4/demo/Demo4Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Demo4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Demo4Application.class, args);
 	}
 }
